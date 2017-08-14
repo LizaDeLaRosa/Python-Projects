706969eb348946ae1751dcdb1559c22c1e0f570d
eddiequezada <31012686+eddiequezada@users.noreply.github.com>
2017-08-14 10:55:40 -0600

    Create Story.py
    this is my first story project for computer programming. the story is incomplete.
    -needs more endings
    -needs more options

---
+++ Story.py
@@ -0,0 +1,5 @@
+def begin_story():
+  user_name = input("Please enter your name")
+  print('You are a bandit and have just escaped prison and ran into an alley way that splits in three directions. Which way do you go?')
+  print('Enter the number that corresponds to your decision')
+  user_response = int(input('1. You decide to go right \n2. Your instincts tell you, you should go left \n3. You feel a strange vibe coming from the path straight ahead '))
